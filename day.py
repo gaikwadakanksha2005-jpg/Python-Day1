9de01b62491c3d67f03a5094221d1801f6459efb
Akanksha <gaikwadAkanksha2005@gmail.com>
2026-09-11 15:30:16 +0530

    python

---
+++ day.py
@@ -0,0 +1,16 @@
+day=int(input("Enter a number:"))
+if day == 1:
+    print("sunday")
+elif day == 2:
+    print("monday") 
+elif day == 3:
+    print("tuesday")
+elif day == 4:
+    print("wed")         
+elif day == 5:
+    print("thu")
+elif day == 6:
+    print("fri") 
+elif day == 7:
+     print("sat")            
+  
